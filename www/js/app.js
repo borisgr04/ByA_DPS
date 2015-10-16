5f634f1d27eb7a753b7f3ffa7a7f57fdb3f54c22
Carlos Tirado <orley333@gmail.com>
2015-10-16 12:56:54 -0500

    Primero commit

---
+++ www/js/app.js
@@ -0,0 +1,118 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+          url: '/app',
+          abstract: true,
+          templateUrl: 'templates/menu.html',
+      })
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/home.html'
+            }
+        }
+    })
+    .state('app.identificar_persona', {
+        url: '/identificar_persona',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/identificar_persona.html'
+            }
+        }
+    })
+    .state('app.pregunta_validacion', {
+        url: '/pregunta_validacion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/pregunta_validacion.html'
+            }
+        }
+    })
+    .state('app.programas_inscritos', {
+        url: '/programas_inscritos',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/programas_inscritos.html'
+            }
+        }
+    })
+    .state('app.menu-familias-en-accion', {
+        url: '/menu-familias-en-accion',
+        views: {
+            'menuContent': {
+                controller: 'MenuFamiliasEnAccionCtrl',
+                templateUrl: 'templates/menu-familias-en-accion.html'
+            }
+        }
+    })
+    .state('app.datos-familia', {
+        url: '/datos-familia',
+        views: {
+            'menuContent': {
+                controller: 'EstadoFamiliaCtrl',
+                templateUrl: 'templates/datos-familia.html'
+            }
+        }
+    })
+    .state('app.novedades', {
+        url: '/novedades',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/novedades.html'
+            }
+        }
+    })
+    .state('app.cumplimiento', {
+        url: '/cumplimiento',
+        views: {
+            'menuContent': {
+                controller: 'CumplimientoCtrl',
+                templateUrl: 'templates/cumplimiento.html'
+            }
+        }
+    })
+    .state('app.antifraude', {
+        url: '/antifraude',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/antifraude.html'
+            }
+        }
+    })
+    .state('app.liquidacionypagos', {
+        url: '/liquidacionypagos',
+        views: {
+            'menuContent': {
+                controller: 'LiquidacionYPagoCtrl',
+                templateUrl: 'templates/liquidacionesypagos.html'
+            }
+        }
+    })
+    ;
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,101 @@
+angular.module('starter.controllers', [])
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {})
+.controller('CumplimientoCtrl', function ($scope) {
+    $scope.groups = [];
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {     
+    return $scope.shownGroup === group;  
+  };
+  
+  _llenarGrupos();
+  function _llenarGrupos(){
+      var e = {};
+      e.name = "Periodo Cumplimiento Salud";
+      e.fecha1 = "11-11-2015";
+      e.name2 = "Periodo Cumplimiento Educación";
+      e.fecha2 = "12-12-2015";
+      e.items = ["Beneficiario","Tipo Inscripción","Cumplió","Fecha Cumplimiento"];
+      
+      var d = {};
+      d.name = "Periodo Cumplimiento Salud";
+      d.fecha1 = "11-11-2015"
+      d.name2 = "Periodo Cumplimiento Educación";
+      d.fecha2 = "12-12-2015";
+      d.items = ["Beneficiario","Tipo Inscripción","Cumplió","Fecha Cumplimiento"];
+      
+      $scope.groups.push(e);
+      $scope.groups.push(d);
+  };
+})
+.controller('LiquidacionYPagoCtrl', function ($scope) {
+    $scope.groups = [];
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {     
+    return $scope.shownGroup === group;  
+  };
+  
+  _llenarGrupos();
+  function _llenarGrupos(){
+      var e = {};
+      e.name = "Periodo Cumplimiento Salud";
+      e.fecha1 = "11-11-2015";
+      e.name2 = "Periodo Cumplimiento Educación";
+      e.fecha2 = "12-12-2015";
+      e.items = ["Beneficiario","Tipo Inscripción","Cumplió","Fecha Cumplimiento"];
+      
+      var d = {};
+      d.name = "Periodo Cumplimiento Salud";
+      d.fecha1 = "11-11-2015"
+      d.name2 = "Periodo Cumplimiento Educación";
+      d.fecha2 = "12-12-2015";
+      d.items = ["Beneficiario","Tipo Inscripción","Cumplió","Fecha Cumplimiento"];
+      
+      $scope.groups.push(e);
+      $scope.groups.push(d);
+  };
+})
+.controller('MenuFamiliasEnAccionCtrl', function ($scope) {
+  $scope._goTo = function(value){
+    window.location.href=value;
+  };
+})
+.controller('EstadoFamiliaCtrl', function ($scope) {
+  $scope.groups = [];
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+  
+  _llenarGrupos();
+  function _llenarGrupos(){
+      var e = {};
+      e.name = "Nombre Miembro";
+      e.items = ["Estado del Beneficiario","Targeta de Identidad","12345678","Camilo Perez","Nombre del Colegio","Priorizado","Graduado","Datos de Salud"];
+     
+      var d = {};
+      d.name = "Nombre Miembro";
+      d.items = ["Estado del Beneficiario","Targeta de Identidad","12345678","Camilo Perez","Nombre del Colegio","Priorizado","Graduado","Datos de Salud"];
+      
+      $scope.groups.push(e);
+      $scope.groups.push(d);
+  };
+});
+
